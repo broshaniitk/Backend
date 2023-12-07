f183492151400911752ff49f74d37f25976402f9
Gulshan kumar <gulshankumaarrr@mail.com>
2023-12-08 03:06:41 +0530

    is ohdd

--- API.py
+++ API.py
@@ -23,4 +23,4 @@ app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
 
 @app.get("/")
 def health_check():
-    return JSONResponse(content={"status": "Running! jhsdjasj"})
+    return JSONResponse(content={"status": "Running! jdsfjsgfjhsbfkejfhejfekjfeui"})
