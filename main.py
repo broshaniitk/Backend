752f9039ece2fc9b64058a230922d6facfea9697
Gulshan kumar <gulshankumaarrr@mail.com>
2023-12-08 23:36:56 +0530

    changes

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
 
 @app.get("/")
 def health_check():
-    return JSONResponse(content={"status": "Hello worldhjhhgghgh"})
+    return JSONResponse(content={"status": "Hellhhhhhhhhh"})
